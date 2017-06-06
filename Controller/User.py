b0fe8f05351834f09c42c873c9a63e3aa6ea2772
Vashanin <vashanin7@gmail.com>
2017-06-07 01:46:25 +0300

    Add authentication

---
+++ Controller/User.py
@@ -0,0 +1,78 @@
+import sqlite3 as sqlite
+
+class User:
+    database = "templates/database/info.db"
+    table = "Users"
+
+    def __init__(self, email, password, name, surname):
+        self.email = email
+        self.password = password
+        self.name = name
+        self.surname = surname
+
+    @classmethod
+    def add_user(cls, user, rights):
+        db = sqlite.connect(cls.database)
+        db.row_factory = sqlite.Row
+
+        with db:
+            conn = db.cursor()
+
+            id = 1
+            try:
+                conn.execute("SELECT MAX(id) FROM {}".format(cls.table))
+                db.commit()
+
+                max_id = conn.fetchall()
+                id = max_id[0][0] + 1
+            except Exception as e:
+                print("Inserting into empty table: " + cls.table + " new index equals " + str(id))
+
+            info = (id, user.email, user.password, user.name, user.surname, rights)
+
+            conn.executemany(
+                "INSERT INTO {} (id, email, password, name, surname, rights) VALUES (?,?,?,?,?,?)"
+                .format(cls.table), info)
+            db.commit()
+
+
+    @classmethod
+    def init_table(cls):
+        db = sqlite.connect(cls.database)
+        db.row_factory = sqlite.Row
+
+        with db:
+            conn = db.cursor()
+            users = (
+                # id, email, password, name, surname, rights
+                (1, "vashanin7@gmail.com", "1111", "Vlad", "Shanin", "admin"),
+                (2, "sirko@gmail.com", "3333", "Serhiy", "Mugilivskiy", "teacher"),
+                (3, "timonov@gmail.com", "2222", "Alex", "Timonov", "student")
+            )
+
+            conn.execute("DROP TABLE {}".format(cls.table))
+            db.commit()
+
+            conn.execute("CREATE TABLE {} (id INTEGER, email TEXT, password TEXT, name TEXT, surname TEXT, rights TEXT)"
+                         .format(cls.table))
+            db.commit()
+
+            conn.executemany("INSERT INTO {} (id, email, password, name, surname, rights) VALUES (?,?,?,?,?,?)"
+                             .format(cls.table), users)
+            db.commit()
+
+    @classmethod
+    def authenticate(cls, email, password):
+        db = sqlite.connect(cls.database)
+        db.row_factory = sqlite.Row
+
+        with db:
+            conn = db.cursor()
+            conn.execute("SELECT * FROM {}".format(cls.table))
+            users = conn.fetchall()
+
+            for user in users:
+                if user[1] == email and user[2] == password:
+                    return True
+
+        return False
--- EduTool.py
+++ EduTool.py
@@ -20,11 +20,11 @@ def login():
     except Exception as e:
         traceback.format_exc()
 
-@app.route("/user_auth/", methods=["POST", "GET"])
+@app.route("/user_auth/", methods=["POST"])
 def user_auth():
     try:
         if request.method == "POST":
-            user_name = request.form["user_name"]
+            user_email = request.form["user_email"]
             user_password = request.form["user_password"]
 
         return redirect(url_for("login"))
@@ -39,6 +39,21 @@ def signup():
         return render_template("signup.html")
     except Exception as e:
         traceback.format_exc()
+
+@app.route("/new_user/", methods=["POST"])
+def new_user():
+    try:
+        if request.method == "POST":
+            email = request.form["email"]
+            password = request.form["password"]
+            name = request.form["name"]
+            surname = request.form["surname"]
+
+        return redirect(url_for("signup"))
+    except Exception as e:
+        print("Exception has been caught: " + e.args[0])
+        traceback.format_exc()
+
 
 if __name__ == '__main__':
     app.run()
