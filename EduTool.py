a7aad63b95dac9252f00a4816387209e09b68d22
Vashanin <vashanin7@gmail.com>
2017-06-07 00:09:11 +0300

    Make web pages

--- EduTool.py
+++ EduTool.py
@@ -13,5 +13,32 @@ def home():
     except Exception as e:
         traceback.format_exc()
 
+@app.route('/login/')
+def login():
+    try:
+        return render_template("login.html")
+    except Exception as e:
+        traceback.format_exc()
+
+@app.route("/user_auth/", methods=["POST", "GET"])
+def user_auth():
+    try:
+        if request.method == "POST":
+            user_name = request.form["user_name"]
+            user_password = request.form["user_password"]
+
+        return redirect(url_for("login"))
+    except Exception as e:
+        print("Exception has been caught: " + e.args[0])
+        traceback.format_exc()
+
+
+@app.route('/signup/')
+def signup():
+    try:
+        return render_template("signup.html")
+    except Exception as e:
+        traceback.format_exc()
+
 if __name__ == '__main__':
     app.run()
